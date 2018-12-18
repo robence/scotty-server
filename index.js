20404998cfd570220f4dff312982bd65f590cda9
rozsabence <robence@gmail.com>
2018-12-18 18:10:00 +0100

    add dependency mongoose and body-parser

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let express = require('express')
 
+let bodyParser = require('body-parser');
+
+let mongoose = require('mongoose');
+
 let app = express();
+
+let apiRoutes = require('./api-routes');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/scotty');
+var db = mongodb.connection;
 
 var port = process.env.PORT || 8080;
 
-app.get('/', (req,res) => res.send('Hello World with Express'));
+app.get('/', (req,res) => res.send('Hello World with Express and Nodemon'));
+
+app.use('/api', apiRoutes);
 
 app.listen(port, function () {
   console.log('Running scotty of port ' + port);
 });
-
-let apiRoutes = require('./api-routes')
-app.use('/api', apiRoutes)
