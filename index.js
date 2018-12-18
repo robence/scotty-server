b500b64b75d576a99ca574b71e868bfb0a9c000b
rozsabence <robence@gmail.com>
2018-12-18 17:54:13 +0100

    express restapi helloworld

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express')
+
+let app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', (req,res) => res.send('Hello World with Express'));
+
+app.listen(port, function () {
+  console.log('Running scotty of port ' + port);
+});
