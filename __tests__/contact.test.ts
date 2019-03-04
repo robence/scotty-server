55e0c0f78c67d7b2511dbe92b6ccb161b8b9b5d0
rozsabence <robence@gmail.com>
2019-03-04 09:14:52 +0100

    add tests for /api/contacts/:id

--- __tests__/contact.test.ts
+++ __tests__/contact.test.ts
@@ -85,89 +85,89 @@ describe('Contact', () => {
     });
   });
 
-  // describe('GET /api/contacts/{id}', () => {
-  //   it('should retrieve a contact', async (done) => {
-  //     const newContact = {
-  //       email: 'superman@email.com',
-  //       username: 'clarkkentoriginal',
-  //     };
-  //
-  //     const res = await Contact.insertMany([newContact]);
-  //
-  //     request(app)
-  //       .get(`/api/contacts/${res._id}`)
-  //       .expect(200)
-  //       .end((err) => {
-  //         if (err) {
-  //           return done(err);
-  //         }
-  //         Contact.findOne({ email: newContact.email }).then((contact) => {
-  //           expect(contact).to.exist;
-  //           expect(contact._id).to.exist;
-  //           expect(contact.username).to.equal(newContact.username);
-  //           done();
-  //         });
-  //       });
-  //   });
-  // });
-  //
-  // describe('PUT /api/contacts/{id}', () => {
-  //   it('should update a contact', async (done) => {
-  //     const newContact = {
-  //       email: 'superman@email.com',
-  //       username: 'clarkkentoriginal',
-  //     };
-  //
-  //     const updateContact = {
-  //       username: 'janedoeoriginal',
-  //     };
-  //
-  //     const res = await Contact.insertMany([newContact]);
-  //
-  //     request(app)
-  //       .put(`/api/contacts/${res[0]._id}`)
-  //       .send(updateContact)
-  //       .expect(200)
-  //       .end((err) => {
-  //         if (err) {
-  //           return done(err);
-  //         }
-  //         Contact.findOne({ _id: res[0]._id }).then((contact) => {
-  //           expect(contact).to.exist;
-  //           expect(contact.email).to.equal(newContact.email);
-  //           expect(contact.username).to.equal(updateContact.username);
-  //           done();
-  //         });
-  //       });
-  //   });
-  // });
-  //
-  // describe('DELETE /api/contacts/{id}', () => {
-  //   it('should delete a contact', async (done) => {
-  //     const newContact = {
-  //       email: 'superman@email.com',
-  //       username: 'clarkkentoriginal',
-  //     };
-  //
-  //     const res = await Contact.insertMany([newContact]);
-  //     const { _id } = res[0];
-  //     const contact = await Contact.findOne(_id);
-  //
-  //     expect(contact).to.exist;
-  //     expect(contact._id).to.exist;
-  //
-  //     request(app)
-  //       .delete(`/api/contacts/${_id}`)
-  //       .expect(200)
-  //       .end((err) => {
-  //         if (err) {
-  //           return done(err);
-  //         }
-  //         Contact.findOne(_id).then((contactt) => {
-  //           expect(contactt).to.not.exist;
-  //           done();
-  //         });
-  //       });
-  //   });
-  // });
+  describe('GET /api/contacts/{id}', () => {
+    it('should retrieve a contact', async (done) => {
+      const newContact = {
+        email: 'example@email.com',
+        username: 'johndoe73',
+      };
+
+      const res = await Contact.insertMany([newContact]);
+
+      request(app)
+        .get(`/api/contacts/${res._id}`)
+        .expect(200)
+        .end((err) => {
+          if (err) {
+            return done(err);
+          }
+          Contact.findOne({ email: newContact.email }).then((contact) => {
+            expect(contact).to.exist;
+            expect(contact._id).to.exist;
+            expect(contact.username).to.equal(newContact.username);
+            done();
+          });
+        });
+    });
+  });
+
+  describe('PUT /api/contacts/{id}', () => {
+    it('should update a contact', async (done) => {
+      const newContact = {
+        email: 'example@email.com',
+        username: 'johndoe73',
+      };
+
+      const updateContact = {
+        username: 'janedoeoriginal',
+      };
+
+      const res = await Contact.insertMany([newContact]);
+
+      request(app)
+        .put(`/api/contacts/${res[0]._id}`)
+        .send(updateContact)
+        .expect(200)
+        .end((err) => {
+          if (err) {
+            return done(err);
+          }
+          Contact.findOne({ _id: res[0]._id }).then((contact) => {
+            expect(contact).to.exist;
+            expect(contact.email).to.equal(newContact.email);
+            expect(contact.username).to.equal(updateContact.username);
+            done();
+          });
+        });
+    });
+  });
+
+  describe('DELETE /api/contacts/{id}', () => {
+    it('should delete a contact', async (done) => {
+      const newContact = {
+        email: 'example@email.com',
+        username: 'johndoe73',
+      };
+
+      const res = await Contact.insertMany([newContact]);
+      const { _id } = res[0];
+      const contact = await Contact.findOne(_id);
+
+      expect(contact).to.exist;
+      expect(contact._id).to.exist;
+
+      request(app)
+        .delete(`/api/contacts/${_id}`)
+        .expect(200)
+        .end((err) => {
+          if (err) {
+            return done(err);
+          }
+          Contact.findOne(_id).then((contactt) => {
+            expect(contactt).to.not.exist;
+            done();
+          });
+        });
+    });
+  });
 });
